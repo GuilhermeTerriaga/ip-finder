39125bd7453ee24bd7b97631e6e5f51569b4b13e
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2026-03-24 01:45:42 -0300

    feature: add NameServer Lookup

--- app/app.go
+++ app/app.go
@@ -13,17 +13,24 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "ip finder"
 	app.Usage = "busca IPs e name servers na internet"
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
 
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "hostname",
+			Usage:  "Busca hostnames de endereços na internet",
+			Flags:  flags,
+			Action: buscarHostnames,
+		},
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
 		},
 	}
@@ -40,4 +47,15 @@ func buscarIps(c *cli.Context) {
 		fmt.Println(ip)
 	}
 
+}
+
+func buscarHostnames(c *cli.Context) {
+	host := c.String("host")
+	servers, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
 }
